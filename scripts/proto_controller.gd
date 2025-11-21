528d2760bf3318cef03f3de6aa0a2aeb1af09b7d
yespeeps <jefboris78@gmail.com>
2025-11-21 21:03:47 +0700

    added a wall

--- scripts/proto_controller.gd
+++ scripts/proto_controller.gd
@@ -1,178 +1,139 @@
-# ProtoController v1.0 by Brackeys
-# CC0 License
-# Intended for rapid prototyping of first-person games.
-# Happy prototyping!
-
 extends CharacterBody3D
 
-## Can we move around?
-@export var can_move : bool = true
-## Are we affected by gravity?
-@export var has_gravity : bool = true
-## Can we press to jump?
-@export var can_jump : bool = true
-## Can we hold to run?
-@export var can_sprint : bool = false
-## Can we press to enter freefly mode (noclip)?
-@export var can_freefly : bool = false
+@onready var head: Node3D = $neck/Head
+@onready var eyes: Node3D = $neck/Head/eyes
+@onready var neck: Node3D = $neck
+@onready var camera: Node3D = $neck/Head/eyes/Camera3D
 
-@export_group("Speeds")
-## Look around rotation speed.
-@export var look_speed : float = 0.002
-## Normal speed.
-@export var base_speed : float = 7.0
-## Speed of jump.
-@export var jump_velocity : float = 4.5
-## How fast do we run?
-@export var sprint_speed : float = 10.0
-## How fast do we freefly?
-@export var freefly_speed : float = 25.0
+@onready var standing_collider = $Standing_Collider
+@onready var crouching_collider = $Crouching_Collider
+@onready var raycast = $RayCast3D
+@onready var feet = $feet
 
-@export_group("Input Actions")
-## Name of Input Action to move Left.
-@export var input_left : String = "input_left"
-## Name of Input Action to move Right.
-@export var input_right : String = "input_right"
-## Name of Input Action to move Forward.
-@export var input_forward : String = "input_forward"
-## Name of Input Action to move Backward.
-@export var input_back : String = "input_back"
-## Name of Input Action to Jump.
-@export var input_jump : String = "ui_accept"
-## Name of Input Action to Sprint.
-@export var input_sprint : String = "sprint"
-## Name of Input Action to toggle freefly mode.
-@export var input_freefly : String = "freefly"
+@export var current_speed = 5.0
+@export var jump_velocity = 4.5
+@export var walking_speed = 5.0
+@export var sprinting_speed = 10.0
+@export var crouching_speed = 3.0
+@export var mouse_sens = 0.4
+@export var lerp_speed = 10.0
+@export var crouching_depth : float = -0.5
 
-var mouse_captured : bool = false
-var look_rotation : Vector2
-var move_speed : float = 0.0
-var freeflying : bool = false
+var direction = Vector3.ZERO
+var input_dir = Vector2.ZERO
 
-## IMPORTANT REFERENCES
-@onready var head: Node3D = $Head
-@onready var collider: CollisionShape3D = $Collider
+const head_bobbing_sprinting_speed = 22
+const head_bobbing_walking_speed = 14
+const head_bobbing_crouching_speed = 10
+
+const head_bobbing_sprinting_intensity = 0.2 #* 100
+const head_bobbing_crouching_intensity = 0.05# * 100
+const head_bobbing_walking_intensity = 0.1 #* 100
+
+var head_bobbing_vector = Vector2.ZERO
+var head_bobbing_index = 0.0
+var head_bobbing_current_intensity = 0.0
+
+# states
+var sprinting = false
+var walking = false
+var crouching = false 
+var sliding = false 
+var leaning = false
+var midair = false
 
 func _ready() -> void:
-	check_input_mappings()
-	look_rotation.y = rotation.y
-	look_rotation.x = head.rotation.x
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
-func _unhandled_input(event: InputEvent) -> void:
-	# Mouse capturing
-	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-		capture_mouse()
-	if Input.is_key_pressed(KEY_ESCAPE):
-		release_mouse()
-	
-	# Look around
-	if mouse_captured and event is InputEventMouseMotion:
-		rotate_look(event.relative)
-	
-	# Toggle freefly mode
-	if can_freefly and Input.is_action_just_pressed(input_freefly):
-		if not freeflying:
-			enable_freefly()
-		else:
-			disable_freefly()
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		rotate_y(deg_to_rad(-event.relative.x * mouse_sens))
+		head.rotate_x(deg_to_rad(-event.relative.y * mouse_sens))
+		head.rotation.x = clamp(head.rotation.x, deg_to_rad(-90), deg_to_rad(90))
 
 func _physics_process(delta: float) -> void:
-	# If freeflying, handle freefly and nothing else
-	if can_freefly and freeflying:
-		var input_dir := Input.get_vector(input_left, input_right, input_forward, input_back)
-		var motion := (head.global_basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-		motion *= freefly_speed * delta
-		move_and_collide(motion)
-		return
-	
-	# Apply gravity to velocity
-	if has_gravity:
-		if not is_on_floor():
-			velocity += get_gravity() * delta
+	if is_on_wall():
+		velocity.y = velocity.y/3.5
 
-	# Apply jumping
-	if can_jump:
-		if Input.is_action_just_pressed(input_jump) and is_on_floor():
-			velocity.y = jump_velocity
+	if Input.is_action_pressed("crouch"):
+		current_speed = lerp(current_speed, crouching_speed, delta*lerp_speed)
+		head.position.y = lerp(head.position.y, crouching_depth, delta*lerp_speed)
+		standing_collider.disabled = true
+		crouching_collider.disabled = false
 
-	# Modify speed based on sprinting
-	if can_sprint and Input.is_action_pressed(input_sprint):
-			move_speed = sprint_speed
-	else:
-		move_speed = base_speed
-
-	# Apply desired movement to velocity
-	if can_move:
-		var input_dir := Input.get_vector(input_left, input_right, input_forward, input_back)
-		var move_dir := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-		if move_dir:
-			velocity.x = move_dir.x * move_speed
-			velocity.z = move_dir.z * move_speed
+		crouching = true
+		sprinting = false
+		walking = false
+	elif !raycast.is_colliding():
+		head.position.y = lerp(head.position.y, 0.0, delta*lerp_speed)    
+		standing_collider.disabled = false
+		crouching_collider.disabled = true
+		if Input.is_action_pressed("sprint"):
+			current_speed = lerp(current_speed, sprinting_speed, delta*lerp_speed)
+			sprinting = true 
+			walking = false
+			crouching = false
 		else:
-			velocity.x = move_toward(velocity.x, 0, move_speed)
-			velocity.z = move_toward(velocity.z, 0, move_speed)
+			current_speed = lerp(current_speed, walking_speed, delta*lerp_speed)
+			sprinting = false 
+			if input_dir == Vector2.ZERO:
+				walking = false
+			else:
+				walking = true
+			crouching = false
+
+	if sprinting:
+		head_bobbing_current_intensity = head_bobbing_sprinting_intensity
+		head_bobbing_index += head_bobbing_sprinting_speed*delta
+	elif walking:
+		head_bobbing_current_intensity = head_bobbing_walking_intensity
+		head_bobbing_index += head_bobbing_walking_speed*delta
+	elif crouching:
+		head_bobbing_current_intensity = head_bobbing_crouching_intensity
+		head_bobbing_index += head_bobbing_crouching_speed*delta
+
+	if is_on_floor() && input_dir != Vector2.ZERO && !leaning:
+		head_bobbing_vector.y = sin(head_bobbing_index)
+		head_bobbing_vector.x = sin(head_bobbing_index/2.0) + 0.5  # sin(head_bobbing_index/2) + 0.5
+
+		eyes.position.y = lerp(eyes.position.y, head_bobbing_vector.y*(head_bobbing_current_intensity/2.0), lerp_speed*delta)
+		eyes.position.x = lerp(eyes.position.x, head_bobbing_vector.x*head_bobbing_current_intensity, lerp_speed*delta)
 	else:
-		velocity.x = 0
-		velocity.y = 0
-	
-	# Use velocity to actually move
+		eyes.position.y = lerp(eyes.position.y, 0.0, lerp_speed*delta)
+		eyes.position.x = lerp(eyes.position.x, 0.0, lerp_speed*delta)
+
+
+# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+		midair = true
+
+# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_velocity
+
+# Get the input direction and handle the movement/deceleration.
+# As good practice, you should replace UI actions with custom gameplay actions.
+	input_dir = Input.get_vector("input_left", "input_right", 'input_forward', "input_back")
+	direction = transform.basis * Vector3(input_dir.x, 0, input_dir.y).normalized()
+
+	if direction:
+		velocity.x = lerp(velocity.x, direction.x * current_speed, delta*lerp_speed)
+		velocity.z = lerp(velocity.z, direction.z * current_speed, delta*lerp_speed)
+	else:
+		velocity.x = lerp(velocity.x, 0.0, delta*lerp_speed)
+		velocity.z = lerp(velocity.z, 0.0, delta*lerp_speed)
+
 	move_and_slide()
 
 
-## Rotate us to look around.
-## Base of controller rotates around y (left/right). Head rotates around x (up/down).
-## Modifies look_rotation based on rot_input, then resets basis and rotates by look_rotation.
-func rotate_look(rot_input : Vector2):
-	look_rotation.x -= rot_input.y * look_speed
-	look_rotation.x = clamp(look_rotation.x, deg_to_rad(-85), deg_to_rad(85))
-	look_rotation.y -= rot_input.x * look_speed
-	transform.basis = Basis()
-	rotate_y(look_rotation.y)
-	head.transform.basis = Basis()
-	head.rotate_x(look_rotation.x)
+func _on_feet_body_entered(body: Node3D) -> void:
+	if body.is_in_group('Box'):
+		body.collision_layer = 1
+		body.collision_mask = 1
 
 
-func enable_freefly():
-	collider.disabled = true
-	freeflying = true
-	velocity = Vector3.ZERO
-
-func disable_freefly():
-	collider.disabled = false
-	freeflying = false
-
-
-func capture_mouse():
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	mouse_captured = true
-
-
-func release_mouse():
-	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-	mouse_captured = false
-
-
-## Checks if some Input Actions haven't been created.
-## Disables functionality accordingly.
-func check_input_mappings():
-	if can_move and not InputMap.has_action(input_left):
-		push_error("Movement disabled. No InputAction found for input_left: " + input_left)
-		can_move = false
-	if can_move and not InputMap.has_action(input_right):
-		push_error("Movement disabled. No InputAction found for input_right: " + input_right)
-		can_move = false
-	if can_move and not InputMap.has_action(input_forward):
-		push_error("Movement disabled. No InputAction found for input_forward: " + input_forward)
-		can_move = false
-	if can_move and not InputMap.has_action(input_back):
-		push_error("Movement disabled. No InputAction found for input_back: " + input_back)
-		can_move = false
-	if can_jump and not InputMap.has_action(input_jump):
-		push_error("Jumping disabled. No InputAction found for input_jump: " + input_jump)
-		can_jump = false
-	if can_sprint and not InputMap.has_action(input_sprint):
-		push_error("Sprinting disabled. No InputAction found for input_sprint: " + input_sprint)
-		can_sprint = false
-	if can_freefly and not InputMap.has_action(input_freefly):
-		push_error("Freefly disabled. No InputAction found for input_freefly: " + input_freefly)
-		can_freefly = false
+func _on_feet_body_exited(body: Node3D) -> void:
+	if body.is_in_group('Box'):
+		body.collision_layer = 2
+		body.collision_mask = 2
---
+++ scripts/source_controller.gd
@@ -0,0 +1,113 @@
+extends CharacterBody3D
+@onready var world_model = $WorldModel
+@onready var camera = $Head/Camera3D
+
+@export var look_sens : float = 0.006
+@export var jump_velocity := 6.0
+@export var auto_bhop := 6.0
+
+# Air movement settings. Need to tweak these to get the feeling dialed in.
+@export var air_cap := 0.85 # Can surf steeper ramps if this is higher, makes it easier to stick and bhop
+@export var air_accel := 800.0
+@export var air_move_speed := 500.0
+
+# Ground movement settings
+@export var walk_speed := 7
+@export var sprint_speed := 8.5
+@export var ground_accel := 14.0
+@export var ground_decel := 10.0
+@export var ground_friction := 6.0
+
+@export var health := 100.0
+@export var max_health := 100.0
+const headbob_move_amount = 0.06
+const headbob_move_frequency = 2.4
+var headbob_time = 0.0
+
+var wish_dir := Vector3.ZERO
+
+var gravity = ProjectSettings.get_setting('physics/3d/default_gravity')
+
+func _ready() -> void:
+	for child : VisualInstance3D in world_model.find_children('*', 'VisualInstance3D'):
+		child.set_layer_mask_value(1, false)
+		child.set_layer_mask_value(2, true)
+				
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	elif event.is_action_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
+	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		if event is InputEventMouseMotion:		
+			rotate_y(-event.relative.x * look_sens)
+			camera.rotate_x(-event.relative.y * look_sens)
+			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-90), deg_to_rad(90))
+
+func _headbob_effect(delta : float):
+	headbob_time = delta * self.velocity.length()
+	camera.transform.origin = Vector3(
+		cos(headbob_time * headbob_move_frequency/2) * delta,
+		sin(headbob_time * headbob_move_frequency) * delta,
+		0
+	)
+
+func wall_run():
+	self.velocity.y -= self.velocity.y/3.5
+
+func get_move_speed() -> float:
+	return sprint_speed if Input.is_action_pressed('sprint') else walk_speed
+
+func _handle_air_physics(delta : float) -> void:
+	if is_on_wall() and Input.is_action_pressed('jump'):
+		wall_run()
+		return
+
+	self.velocity.y -= gravity * delta
+
+	var cur_speed_in_wish_dir = self.velocity.dot(wish_dir)
+	var capped_speed = min((air_move_speed * wish_dir).length(), air_cap)
+
+	var add_speed_till_cap = capped_speed - cur_speed_in_wish_dir
+	if add_speed_till_cap > 0:
+		var accel_speed = air_accel * air_move_speed * delta
+		accel_speed = min(accel_speed, add_speed_till_cap)
+		self.velocity += accel_speed * wish_dir
+
+func _handle_ground_physics(delta : float) -> void:
+	var cur_speed_in_wish_dir = self.velocity.dot(wish_dir)
+	var add_speed_till_cap = get_move_speed() - cur_speed_in_wish_dir
+	if add_speed_till_cap > 0:
+		var accel_speed = ground_accel * get_move_speed() * delta
+		accel_speed = min(accel_speed, add_speed_till_cap)
+		self.velocity += accel_speed * wish_dir
+
+	var control = max(self.velocity.length(), ground_decel)
+	var drop = control * ground_friction * delta
+	var new_speed = max(self.velocity.length() - drop, 0.0)
+	if self.velocity.length() > 0:
+		new_speed /= self.velocity.length()
+	self.velocity *= new_speed
+
+	_headbob_effect(delta)
+
+func _physics_process(delta: float) -> void:
+	var input_dir = Input.get_vector('input_left', 'input_right', 'input_back', 'input_forward').normalized()
+
+	wish_dir = self.global_transform.basis * Vector3(input_dir.x, 0, -input_dir.y)
+
+	if is_on_floor():
+		if Input.is_action_just_pressed('jump') or (auto_bhop and Input.is_action_pressed('jump')):
+			self.velocity.y += jump_velocity
+		_handle_ground_physics(delta)
+	else:
+		_handle_air_physics(delta)
+
+	move_and_slide()
+
+
+
+	
+
+# func _process(delta: float) -> void:
